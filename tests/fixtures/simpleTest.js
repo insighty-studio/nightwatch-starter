145f6d3e133716b09512414f84df3d1933cca08c
Alex Panov <thealexpanov@gmail.com>
2018-03-08 11:24:33 -0500

    Fix the test

--- tests/fixtures/simpleTest.js
+++ tests/fixtures/simpleTest.js
@@ -2,7 +2,7 @@ module.exports = {
   'Github Repositories Navigation' : function (browser) {
     browser
       .url('https://github.com/alexpanov')
-      .waitForElementVisible('nav.underline-nav', 1000)
+      .waitForElementVisible('nav', 1000)
       .clickLinkContainingText('Repositories')
       .waitForElementPresent('#user-repositories-list', 1000)
       .assert.containsText('#user-repositories-list', 'nightwatch-starter')
